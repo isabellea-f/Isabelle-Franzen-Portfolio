c6c2a75a9ad59b4d8a9325d950fc74891fc37ffe
isabelle.amsalu@gmail.com <isabelle.amsalu@gmail.com>
2025-10-03 15:12:47 +0200

    chore: Add scroll position detection for section tracking

--- script.js
+++ script.js
@@ -1,7 +1,41 @@
 const hamburgerButton = document.querySelector("#hamburger");
 const openMenu = document.querySelector("nav ul");
 
+/* Sections */
+const aboutSection = document.querySelector("#about");
+const resumeSection = document.querySelector("#resume");
+const projectsSection = document.querySelector("#projects");
+
 hamburgerButton.addEventListener("click", () => {
   openMenu.classList.toggle("open");
   hamburgerButton.classList.toggle("active");
+});
+
+/* Link highlight on scroll */
+
+window.addEventListener("scroll", () => {
+  const scrollPosition = window.scrollY;
+
+  if (
+    scrollPosition >= aboutSection.offsetTop &&
+    scrollPosition < resumeSection.offsetTop
+  ) {
+    console.log("about");
+  } else if (
+    scrollPosition >= resumeSection.offsetTop &&
+    scrollPosition < resumeSection.offsetTop + resumeSection.offsetHeight
+  ) {
+    console.log("Resume");
+  } else if (
+    scrollPosition >=
+    resumeSection.offsetTop + resumeSection.offsetHeight
+  ) {
+    console.log("projects");
+  }
+  {
+    console.log("projects");
+  }
+  // console.log(aboutSection.offsetTop); 498
+  // resumeSection: 1135
+  // projectsSection 2254
 });
